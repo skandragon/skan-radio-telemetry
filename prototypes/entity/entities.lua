8596acb1726a06c2c919ae1686ff9647e171225b
Michael Graff <explorer@flame.org>
2017-06-03 22:25:22 -0700

    up slot count

--- prototypes/entity/entities.lua
+++ prototypes/entity/entities.lua
@@ -133,7 +133,7 @@ data:extend(
         collision_box = {{-0.7, -0.7}, {0.7, 0.7}},
         selection_box = {{-0.8, -0.8}, {0.8, 0.8}},
         fast_replaceable_group = "skan-radio-equipment",
-        item_slot_count = 20,
+        item_slot_count = 200,
         --energy_source =
         --{
             --type = "electric",
